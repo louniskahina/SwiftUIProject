71bf7665e01c1343ad0124d1b0718986dbd68e11
Kahina Lounis <kahina.lounis@europcar.com>
2021-07-11 23:43:04 +0200

    fc

---
+++ SwiftUIProject-1/SwiftUIProject_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProject_1App.swift
+//  SwiftUIProject-1
+//
+//  Created by Kahina Lounis on 11/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProject_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
